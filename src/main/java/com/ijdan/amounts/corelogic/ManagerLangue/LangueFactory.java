ef3e244a668f469b3fb1405a3bae54482f6d0a04
Ijdan <nadji.berraf@gmail.com>
2024-05-05 19:03:05 +0200

    Refactor LangueFactory.java

--- src/main/java/com/ijdan/amounts/corelogic/ManagerLangue/LangueFactory.java
+++ src/main/java/com/ijdan/amounts/corelogic/ManagerLangue/LangueFactory.java
@@ -4,15 +4,20 @@ import com.ijdan.amounts.corelogic.ManagerLangue.Langues.Anglais;
 import com.ijdan.amounts.corelogic.ManagerLangue.Langues.Francais;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component("reglesParLangueFactory")
 public class LangueFactory {
 
+    private static final Map<String, LangueInterface> LANGUE_MAP = new HashMap<>();
+
+    static {
+        LANGUE_MAP.put("EN", new Anglais());
+        LANGUE_MAP.put("FR", new Francais());
+    }
+
     public LangueInterface donneReglesParLangue (String langue){
-        switch (langue){
-            case "EN":
-                return new Anglais();
-            default:
-                return new Francais();
-        }
+        return LANGUE_MAP.getOrDefault(langue, new Francais());
     }
 }
